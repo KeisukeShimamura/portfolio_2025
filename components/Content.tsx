970f84fad31ccf036c1f161862d83a3a52785c77
k.shimamura <34276109+KeisukeShimamura@users.noreply.github.com>
2025-03-15 15:55:48 +0900

    typographyを用いてデザインを適用

--- components/Content.tsx
+++ components/Content.tsx
@@ -7,7 +7,7 @@ type ContentProps = {
 export default function Content({ content }: ContentProps) {
   return (
     <article
-      className="prose prose-base prose-slate max-w-none mb-4"
+      className="prose prose-sm max-w-none mb-4"
       dangerouslySetInnerHTML={{ __html: content }}
     />
   );
